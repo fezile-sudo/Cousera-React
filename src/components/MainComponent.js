8224b85808d8c7477ccf96eef24ec8f2063d7a2d
Fezile Gulwa <fezilegulwa@gmail.com>
2026-03-31 09:38:56 +0200

    single page application part 1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import Home from './HomeComponent';
 import Menu from './MenuComponent';
+import Contact from './ContactComponent';
 import DishDetail from './DishdetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { LEADERS } from '../shared/leaders';
+import { PROMOTIONS } from '../shared/promotions';
 import { Routes, Route, Navigate } from 'react-router-dom';
 
 class Main extends Component {
@@ -12,7 +16,10 @@ class Main extends Component {
     super(props);
     this.state = {
       dishes: DISHES,
-      selectedDish: null
+      selectedDish: null,
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS
     };
   }
 
@@ -21,7 +28,10 @@ class Main extends Component {
   };
 
   render() {
-    const HomePage = () => <Home />;
+    const HomePage = () => <Home dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+              promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+              leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+              />;
 
     return (
       <div>
@@ -29,7 +39,9 @@ class Main extends Component {
         <Routes>
               <Route path="/home" element={<HomePage />} />
               <Route path="/menu" element={<Menu dishes={this.state.dishes} onClick={this.onDishSelect} />} />
+              <Route path="/contactus" element={<Contact />} />
               <Route path="*" element={<Navigate to="/home" />} />
+              
         </Routes>
         <DishDetail
           dish={this.state.dishes.find(
